5750b35f2299851f94fb6e2bb91f544e1fa02048
dpi <pro@danielph.in>
2018-02-21 17:16:55 +0800

    New explosion utility.

--- lib/Drupal/Component/Utility/Tags.php
+++ lib/Drupal/Component/Utility/Tags.php
@@ -2,6 +2,8 @@
 
 namespace Drupal\Component\Utility;
 
+use Drupal\Core\StringTranslation\TranslatableMarkup;
+
 /**
  * Defines a class that can explode and implode tags.
  *
@@ -70,6 +72,88 @@ class Tags {
       $encoded_tags[] = self::encode($tag);
     }
     return implode(', ', $encoded_tags);
+  }
+
+  public static function explodeWithErrors($string) {
+    $errors = [];
+    $tags = [];
+
+    while (strlen($string) > 0) {
+      preg_match('/^\s*(")/', $string, $matches, PREG_OFFSET_CAPTURE);
+      $inQuotes = count($matches) != 0;
+      if ($inQuotes) {
+        $first_quote_position = $matches[1][1];
+        $string = substr($string, $first_quote_position + 1);
+
+        // Find first single quote.
+        preg_match('/(?:^|[^"])(?:"")*(")(?:[^"]|$)/', $string, $matches, PREG_OFFSET_CAPTURE);
+        $end_quote_position = $matches[1][1] ?? NULL;
+        if (!isset($end_quote_position)) {
+          $errors[] = new TranslatableMarkup('No ending quote character found.');
+          break;
+        }
+
+        $tag = substr($string, 0, $end_quote_position);
+        $tags[] = $tag;
+        $string = substr($string, $end_quote_position + 1);
+
+        // Next char should be whitespace then comma or end of string
+        // Otherwise error: "Found text after $lasttag, expected comma or end of text."
+        preg_match('/^\s*([,]|$)/', $string, $matches, PREG_OFFSET_CAPTURE);
+        $nextChar = $matches[1][0] ?? NULL;
+        if ($nextChar === ',') {
+          // Take off the comma.
+          $string = substr($string, 1);
+        }
+        elseif ($nextChar === '') {
+          // End of string. Finish.
+          break;
+        }
+        else {
+          $errors[] = new TranslatableMarkup('Unexpected text after "@tag". Expected comma or end of text. Found @unexpected.', [
+            '@tag' => $tag,
+            '@unexpected' => substr($string, 0, 10),
+          ]);
+          break;
+        }
+      }
+      else {
+        $end_position = strpos($string, ',');
+        $end_position = $end_position !== FALSE ? $end_position : strlen($string);
+
+        $tag = substr($string, 0, $end_position);
+        $string = substr($string, $end_position + 1);
+        // Determine if there are any single quote characters.
+        preg_match('/[^"](?:"")*(")(?:[^"]|$)/', $tag, $matches);
+        if (!count($matches)) {
+          $tags[] = trim($tag);
+        }
+        else {
+          $errors[] = new TranslatableMarkup('Unexpected quote character found');
+          break;
+        }
+      }
+    }
+
+    // Replace quote pairs with singles.
+    $tags = array_map(
+      function ($item) {
+        return str_replace('""', '"', $item);
+      },
+      $tags
+    );
+
+    // Remove zero length.
+    $tags = array_filter($tags);
+    // Remove duplicates.
+    $tags = array_unique($tags);
+    // Reset keys.
+    $tags = array_values($tags);
+
+    return [
+      'tags' => $tags,
+      'errors' => $errors,
+    ];
   }
 
 }
--- tests/Drupal/Tests/Core/Common/TagsTest.php
+++ tests/Drupal/Tests/Core/Common/TagsTest.php
@@ -56,4 +56,210 @@ class TagsTest extends UnitTestCase {
     }
   }
 
+  /**
+   * Xyz
+   *
+   * @param string $string
+   * @param array $tagsExpected
+   * @param bool $hasError
+   *
+   * @dataProvider providerTestExplode
+   */
+  public function testExplodeXXX($string, $tagsExpected, $hasError) {
+    $result = Tags::explode($string);
+    $tags = $result['tags'];
+    $errors = $result['errors'];
+
+    $this->assertEquals($tagsExpected, $tags);
+    $this->assertEquals($hasError, count($errors) > 0);
+  }
+
+  public function providerTestExplode() {
+    $tests = [];
+
+    $tests['unquoted'] = [
+      'hello',
+      ['hello'],
+      FALSE,
+    ];
+    $tests['unquoted multiword'] = [
+      'Drupal with some spaces',
+      ['Drupal with some spaces'],
+      FALSE,
+    ];
+    $tests['quoted, tag with comma'] = [
+      '"Hello, World"',
+      ['Hello, World'],
+      FALSE,
+    ];
+    $tests['quoted, missing trailing quote'] = [
+      '"Hello',
+      [],
+      TRUE,
+    ];
+    $tests['unquoted, unexpected quote'] = [
+      'Hello"',
+      [],
+      TRUE,
+    ];
+    $tests['unquoted, empty tags'] = [
+      ',,,,,,',
+      [],
+      FALSE,
+    ];
+    $tests['unescaped, empty tags, word, empty tags'] = [
+      ',,hello,,',
+      ['hello'],
+      FALSE,
+    ];
+    $tests['quoted, empty'] = [
+      '"hello",',
+      ['hello'],
+      FALSE,
+    ];
+    $tests['unquoted, tag, empty tag'] = [
+      'hello,',
+      ['hello'],
+      FALSE,
+    ];
+    $tests['unquoted, quoted'] = [
+      'unquoted,"quoted2"',
+      ['unquoted', 'quoted2'],
+      FALSE,
+    ];
+    $tests['unquoted, unquoted'] = [
+      'unquoted,unquoted2',
+      ['unquoted', 'unquoted2'],
+      FALSE,
+    ];
+    $tests['quoted, unquoted'] = [
+      '"quoted",unquoted',
+      ['quoted', 'unquoted'],
+      FALSE,
+    ];
+    $tests['quoted, quoted'] = [
+      '"quoted","quoted2"',
+      ["quoted", "quoted2"],
+      FALSE,
+    ];
+    $tests['empty tag, unquoted'] = [
+      ',hello',
+      ['hello'],
+      FALSE,
+    ];
+    $tests['empty tag, quoted'] = [
+      ',"hello"',
+      ['hello'],
+      FALSE,
+    ];
+    $tests['quoted, unexpected quote'] = [
+      '"Jimmy "The Boss" Smith, Mr"',
+      ['Jimmy '],
+      TRUE,
+    ];
+    $tests['quoted, missing comma, unquoted'] = [
+      '"Hello" World',
+      ['Hello'],
+      TRUE,
+    ];
+    $tests['quoted, missing comma, quoted'] = [
+      '"Hello" "World"',
+      ['Hello'],
+      TRUE,
+    ];
+    $tests['unquoted, unexpected quote, word'] = [
+      'Jimmy "The Boss" Smith, Mr',
+      [],
+      TRUE,
+    ];
+    $tests['Quoted with no contents, missing comma'] = [
+      '""Jimmy "The Boss" Smith, Mr"',
+      [],
+      TRUE,
+    ];
+    $tests['unquoted, unquoted, trailing escaped'] = [
+      'Jimmy The Boss Smith, Mr""',
+      ['Jimmy The Boss Smith', 'Mr"'],
+      FALSE,
+    ];
+    $tests['unquoted, word within escaped, quoted, empty tag, unexpected character'] = [
+      'Jimmy ""The Boss"" Smith, ""Mr""',
+      ['Jimmy "The Boss" Smith'],
+      TRUE,
+    ];
+    $tests['unquoted word within escaped, unquoted'] = [
+      'Jimmy ""The Boss"" Smith, Mr',
+      ['Jimmy "The Boss" Smith', 'Mr'],
+      FALSE,
+    ];
+    $tests['quoted, words, escaped words, word'] = [
+      '"Jimmy ""The Boss"" Smith, Mr"',
+      ['Jimmy "The Boss" Smith, Mr'],
+      FALSE,
+    ];
+    // Ensures the two quotes don't get escaped, rather create empty tag.
+    $tests['quoted, empty, unquoted'] = [
+      '"",hello',
+      ['hello'],
+      FALSE,
+    ];
+    $tests['literal double quote, missing trailing quote'] = [
+      '"""',
+      [],
+      TRUE,
+    ];
+    $tests['quoted, escaped, missing comma'] = [
+      '""""Hello""""',
+      ['"'],
+      TRUE,
+    ];
+    $tests['Escaped quote, tag'] = [
+      '"""",hello',
+      ['"', 'hello'],
+      FALSE,
+    ];
+    $tests['quoted, double escaped, missing trailing quote'] = [
+      '""""",hello',
+      [],
+      TRUE,
+    ];
+    $tests['quoted, two escaped, comma, tag'] = [
+      '"""""",hello',
+      ['""', 'hello'],
+      FALSE,
+    ];
+    $tests['quoted, two escaped, word, two escaped'] = [
+      '"""""Hello"""""',
+      ['""Hello""'],
+      FALSE,
+    ];
+    $tests['unquoted, two words, escaped'] = [
+      'hello ""world""',
+      ['hello "world"'],
+      FALSE,
+    ];
+    $tests['word, escaped, unexpected quote'] = [
+      'hello ""world"',
+      [],
+      TRUE,
+    ];
+    $tests['whitespace around unquoted, whitespace around quoted'] = [
+      '    hello   ,    "world"    ',
+      ['hello', 'world'],
+      FALSE,
+    ];
+    $tests['Outerquotes, no whitespace, multiword, escaped quotes, escaped quotes on end'] = [
+      '"Legendary Drupal mascot of doom: ""Druplicon"""',
+      ['Legendary Drupal mascot of doom: "Druplicon"'],
+      FALSE,
+    ];
+    $tests['Outerquotes, no whitespace, multiword, inner commas'] = [
+      '"Drupal, although it rhymes with sloopal, is as awesome as a troopal!"',
+      ['Drupal, although it rhymes with sloopal, is as awesome as a troopal!'],
+      FALSE,
+    ];
+
+    return $tests;
+  }
+
 }
