06d08c3ca374d25e660ce953fb8a978bf080d22d
dpi <pro@danielph.in>
2020-06-11 14:09:16 +0800

    https://www.drupal.org/files/issues/2019-05-06/2350939-73.patch

--- lib/Drupal/Core/Entity/EntityBase.php
+++ lib/Drupal/Core/Entity/EntityBase.php
@@ -166,7 +166,7 @@ abstract class EntityBase implements EntityInterface {
 
     // Links pointing to the current revision point to the actual entity. So
     // instead of using the 'revision' link, use the 'canonical' link.
-    if (in_array($rel, ['revision', 'revision-revert-form'], TRUE) && $this instanceof RevisionableInterface) {
+    if (in_array($rel, ['revision', 'revision-revert-form'], TRUE) && $this instanceof RevisionableInterface && $this->isDefaultRevision()) {
       $rel = 'canonical';
     }
 
