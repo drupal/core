9801ce493e17fd61cd61bb4b48ade7473f5e785c
jmarcpambrun <jmarc.pambrun@free.fr>
2023-04-01 11:17:01 +0200

    to avoid Deprecated function: strpos(): Passing null to parameter #1 ($haystack) of type string is deprecated in Drupal\Component\Plugin\PluginBase->getBaseId() (line 71 of core\lib\Drupal\Component\Plugin\PluginBase.php). https://www.drupal.org/files/issues/2022-07-07/depricated-strpos-arg-haystack-3294760-01.patch

--- lib/Drupal/Component/Plugin/PluginBase.php
+++ lib/Drupal/Component/Plugin/PluginBase.php
@@ -68,7 +68,7 @@ abstract class PluginBase implements PluginInspectionInterface, DerivativeInspec
    */
   public function getBaseId() {
     $plugin_id = $this->getPluginId();
-    if (strpos($plugin_id, static::DERIVATIVE_SEPARATOR)) {
+    if (strpos( strval($plugin_id), strval(static::DERIVATIVE_SEPARATOR))) {
       [$plugin_id] = explode(static::DERIVATIVE_SEPARATOR, $plugin_id, 2);
     }
     return $plugin_id;
@@ -80,7 +80,7 @@ abstract class PluginBase implements PluginInspectionInterface, DerivativeInspec
   public function getDerivativeId() {
     $plugin_id = $this->getPluginId();
     $derivative_id = NULL;
-    if (strpos($plugin_id, static::DERIVATIVE_SEPARATOR)) {
+    if (strpos(strval($plugin_id), strval(static::DERIVATIVE_SEPARATOR))) {
       [, $derivative_id] = explode(static::DERIVATIVE_SEPARATOR, $plugin_id, 2);
     }
     return $derivative_id;
