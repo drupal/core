b969c010277511579484dd510de057b48c02b8a5
Alex Pott <alex.a.pott@googlemail.com>
2026-03-26 13:39:12 +0000

    task: #3568123 Update INSTALL.txt for Drupal 12
    By: quietone
    By: dcam
    By: smustgrave
    By: longwave

--- modules/pgsql/src/Driver/Database/pgsql/Install/Tasks.php
+++ modules/pgsql/src/Driver/Database/pgsql/Install/Tasks.php
@@ -21,7 +21,7 @@ class Tasks extends InstallTasks {
    *
    * The contrib extension pg_trgm is supposed to be installed.
    *
-   * @see https://www.postgresql.org/docs/16/pgtrgm.html
+   * @see https://www.postgresql.org/docs/18/pgtrgm.html
    */
   const PGSQL_MINIMUM_VERSION = '18';
 
