9468af06ece54273b9b0f57be555b53f921f6454
catch <6915-catch@users.noreply.drupalcode.org>
2026-04-14 10:49:59 +0100

    feat: #3311365 Use PHP attributes for route discovery
    By: alexpott
    By: dpi
    By: andypost
    By: aaronmchale
    By: catch
    By: smustgrave
    By: joachim
    By: donquixote
    By: duadua
    By: kim.pepper
    By: wim leers
    By: longwave
    By: godotislate
    By: benjifisher

--- tests/Drupal/Tests/Core/Routing/RouteBuilderTest.php
+++ tests/Drupal/Tests/Core/Routing/RouteBuilderTest.php
@@ -15,12 +15,16 @@ use Drupal\Core\Routing\RouteBuilder;
 use Drupal\Core\Routing\RouteBuildEvent;
 use Drupal\Core\Routing\RouteCompiler;
 use Drupal\Core\Routing\RoutingEvents;
+use Drupal\Core\Routing\YamlRouteDiscovery;
 use Drupal\Tests\UnitTestCase;
+use PHPUnit\Framework\Attributes\AllowMockObjectsWithoutExpectations;
 use PHPUnit\Framework\Attributes\CoversClass;
 use PHPUnit\Framework\Attributes\Group;
 use PHPUnit\Framework\MockObject\MockObject;
 use PHPUnit\Framework\MockObject\Stub;
-use Prophecy\Argument;
+use PHPUnit\Framework\Attributes\IgnoreDeprecations;
+use Symfony\Component\EventDispatcher\EventDispatcher;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 use Symfony\Component\Routing\Route;
 use Symfony\Component\Routing\RouteCollection;
 
@@ -29,7 +33,7 @@ use Symfony\Component\Routing\RouteCollection;
  */
 #[CoversClass(RouteBuilder::class)]
 #[Group('Routing')]
-class RouteBuilderTest extends UnitTestCase {
+class RouteBuilderTest extends UnitTestCase implements EventSubscriberInterface {
 
   /**
    * The actual tested route builder.
@@ -76,6 +80,21 @@ class RouteBuilderTest extends UnitTestCase {
   protected CheckProviderInterface $checkProvider;
 
   /**
+   * The route collection that the dynamic and alter events expect to receive.
+   */
+  protected RouteCollection $expectedRouteCollection;
+
+  /**
+   * Indicates whether the dynamic event was fired.
+   */
+  protected bool $dynamicFired = FALSE;
+
+  /**
+   * Indicates whether the alter event was fired.
+   */
+  protected bool $alterFired = FALSE;
+
+  /**
    * {@inheritdoc}
    */
   protected function setUp(): void {
@@ -83,8 +102,7 @@ class RouteBuilderTest extends UnitTestCase {
 
     $this->dumper = $this->createStub(MatcherDumperInterface::class);
     $this->lock = $this->createMock('Drupal\Core\Lock\LockBackendInterface');
-    $this->dispatcher = $this->prophesize('\Symfony\Contracts\EventDispatcher\EventDispatcherInterface');
-    $this->dispatcher->dispatch(Argument::cetera(), Argument::cetera())->willReturnArgument(0);
+    $this->dispatcher = new EventDispatcher();
     $this->moduleHandler = $this->createStub(ModuleHandlerInterface::class);
     $this->controllerResolver = $this->createStub(ControllerResolverInterface::class);
     $this->yamlDiscovery = $this->getMockBuilder('\Drupal\Core\Discovery\YamlDiscovery')
@@ -92,8 +110,12 @@ class RouteBuilderTest extends UnitTestCase {
       ->getMock();
     $this->checkProvider = $this->createStub(CheckProviderInterface::class);
 
-    $this->routeBuilder = new TestRouteBuilder($this->dumper, $this->lock, $this->dispatcher->reveal(), $this->moduleHandler, $this->controllerResolver, $this->checkProvider);
-    $this->routeBuilder->setYamlDiscovery($this->yamlDiscovery);
+    $yamlRouteDiscovery = new TestYamlRouteDiscovery($this->moduleHandler, $this->controllerResolver);
+    $yamlRouteDiscovery->setYamlDiscovery($this->yamlDiscovery);
+
+    $this->dispatcher->addSubscriber($this);
+    $this->dispatcher->addSubscriber($yamlRouteDiscovery);
+    $this->routeBuilder = new RouteBuilder($this->dumper, $this->lock, $this->dispatcher, $this->checkProvider);
   }
 
   /**
@@ -118,6 +140,7 @@ class RouteBuilderTest extends UnitTestCase {
    * Tests that the route rebuilding both locks and unlocks.
    */
   public function testRebuildLockingUnlocking(): void {
+    $this->expectedRouteCollection = new RouteCollection();
     $this->lock->expects($this->once())
       ->method('acquire')
       ->with('router_rebuild')
@@ -132,6 +155,8 @@ class RouteBuilderTest extends UnitTestCase {
       ->willReturn([]);
 
     $this->assertTrue($this->routeBuilder->rebuild());
+    $this->assertTrue($this->dynamicFired);
+    $this->assertTrue($this->alterFired);
   }
 
   /**
@@ -154,6 +179,8 @@ class RouteBuilderTest extends UnitTestCase {
       ->method('findAll');
 
     $this->assertFalse($this->routeBuilder->rebuild());
+    $this->assertFalse($this->dynamicFired);
+    $this->assertFalse($this->alterFired);
   }
 
   /**
@@ -181,13 +208,7 @@ class RouteBuilderTest extends UnitTestCase {
     foreach ($route_collection->all() as $route) {
       $route->setOption('compiler_class', RouteCompiler::class);
     }
-    $route_build_event = new RouteBuildEvent($route_collection);
-
-    // Ensure that the alter routes events are fired.
-    $this->dispatcher->dispatch($route_build_event, RoutingEvents::DYNAMIC)
-      ->shouldBeCalled();
-    $this->dispatcher->dispatch($route_build_event, RoutingEvents::ALTER)
-      ->shouldBeCalled();
+    $this->expectedRouteCollection = $route_collection;
 
     // Ensure that access checks are set.
     $this->checkProvider->expects($this->once())
@@ -203,6 +224,8 @@ class RouteBuilderTest extends UnitTestCase {
       ->with();
 
     $this->assertTrue($this->routeBuilder->rebuild());
+    $this->assertTrue($this->dynamicFired);
+    $this->assertTrue($this->alterFired);
   }
 
   /**
@@ -251,14 +274,7 @@ class RouteBuilderTest extends UnitTestCase {
     $route_collection_filled = new RouteCollection();
     $route_collection_filled->add('test_route.1', new Route('/test-route/1'));
     $route_collection_filled->add('test_route.2', new Route('/test-route/2'));
-
-    $route_build_event = new RouteBuildEvent($route_collection_filled);
-
-    // Ensure that the alter routes events are fired.
-    $this->dispatcher->dispatch($route_build_event, RoutingEvents::DYNAMIC)
-      ->shouldBeCalled();
-    $this->dispatcher->dispatch($route_build_event, RoutingEvents::ALTER)
-      ->shouldBeCalled();
+    $this->expectedRouteCollection = $route_collection_filled;
 
     // Ensure that access checks are set.
     $this->checkProvider->expects($this->once())
@@ -273,12 +289,15 @@ class RouteBuilderTest extends UnitTestCase {
       ->method('dump');
 
     $this->assertTrue($this->routeBuilder->rebuild());
+    $this->assertTrue($this->dynamicFired);
+    $this->assertTrue($this->alterFired);
   }
 
   /**
    * Tests \Drupal\Core\Routing\RouteBuilder::rebuildIfNeeded() method.
    */
   public function testRebuildIfNeeded(): void {
+    $this->expectedRouteCollection = new RouteCollection();
     $this->lock->expects($this->once())
       ->method('acquire')
       ->with('router_rebuild')
@@ -296,9 +315,15 @@ class RouteBuilderTest extends UnitTestCase {
 
     // This will trigger a successful rebuild.
     $this->assertTrue($this->routeBuilder->rebuildIfNeeded());
+    $this->assertTrue($this->dynamicFired);
+    $this->assertTrue($this->alterFired);
 
+    $this->dynamicFired = FALSE;
+    $this->alterFired = FALSE;
     // This will not trigger a rebuild.
     $this->assertFalse($this->routeBuilder->rebuildIfNeeded());
+    $this->assertFalse($this->dynamicFired);
+    $this->assertFalse($this->alterFired);
   }
 
   /**
@@ -334,13 +359,34 @@ class RouteBuilderTest extends UnitTestCase {
     $route_collection_filled = new RouteCollection();
     $route_collection_filled->add('test_route.override', new Route('/test_route_override', [], [], ['compiler_class' => 'Class\Does\Not\Exist']));
     $route_collection_filled->add('test_route', new Route('/test_route', [], [], ['compiler_class' => RouteCompiler::class]));
-    $route_build_event = new RouteBuildEvent($route_collection_filled);
-    $this->dispatcher->dispatch($route_build_event, RoutingEvents::DYNAMIC)
-      ->shouldBeCalled();
-    $this->dispatcher->dispatch($route_build_event, RoutingEvents::ALTER)
-      ->shouldBeCalled();
+    $this->expectedRouteCollection = $route_collection_filled;
 
     $this->assertTrue($this->routeBuilder->rebuild());
+    $this->assertTrue($this->dynamicFired);
+    $this->assertTrue($this->alterFired);
+  }
+
+  #[IgnoreDeprecations]
+  #[AllowMockObjectsWithoutExpectations]
+  public function testDeprecatedConstructorArgs(): void {
+    $this->expectUserDeprecationMessage('Calling Drupal\Core\Routing\RouteBuilder::__construct() with the module handler and controller resolver services is deprecated in drupal:11.4.0 and will be removed in drupal:12.0.0. See https://www.drupal.org/node/3324751');
+    new RouteBuilder($this->dumper, $this->lock, $this->dispatcher, $this->moduleHandler, $this->controllerResolver, $this->checkProvider);
+  }
+
+  public function onRouteDynamic(RouteBuildEvent $event): void {
+    $this->dynamicFired = TRUE;
+    $this->assertEquals($event->getRouteCollection(), $this->expectedRouteCollection);
+  }
+
+  public function onRouteAlter(RouteBuildEvent $event): void {
+    $this->alterFired = TRUE;
+    $this->assertEquals($event->getRouteCollection(), $this->expectedRouteCollection);
+  }
+
+  public static function getSubscribedEvents(): array {
+    $events[RoutingEvents::DYNAMIC] = ['onRouteDynamic'];
+    $events[RoutingEvents::ALTER] = ['onRouteAlter'];
+    return $events;
   }
 
 }
@@ -348,7 +394,7 @@ class RouteBuilderTest extends UnitTestCase {
 /**
  * Extends the core route builder with a setter method for the YAML discovery.
  */
-class TestRouteBuilder extends RouteBuilder {
+class TestYamlRouteDiscovery extends YamlRouteDiscovery {
 
   /**
    * The mocked YAML discovery.
