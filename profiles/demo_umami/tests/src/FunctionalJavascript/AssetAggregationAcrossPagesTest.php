0041d5bad696d3e74cc5a2e48759041711d6eb75
catch <6915-catch@users.noreply.drupalcode.org>
2026-04-29 12:49:20 +0100

    fix: follow-up #3502993 to adjust AssetAggregationAcrossPagesTest.

--- profiles/demo_umami/tests/src/FunctionalJavascript/AssetAggregationAcrossPagesTest.php
+++ profiles/demo_umami/tests/src/FunctionalJavascript/AssetAggregationAcrossPagesTest.php
@@ -110,7 +110,7 @@ class AssetAggregationAcrossPagesTest extends PerformanceTestBase {
     $expected = [
       'ScriptCount' => 15,
       'ScriptBytes' => 3222537,
-      'StylesheetCount' => 8,
+      'StylesheetCount' => 7,
       'StylesheetBytes' => 1386351,
     ];
     $this->assertMetrics($expected, $performance_data);
